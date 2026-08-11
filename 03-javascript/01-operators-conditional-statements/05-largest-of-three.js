3d854cc52ef1107cd1886542d064650f8162e491
Parmarth Kumar <parmarthk26@gmail.com>
2026-08-11 22:10:49 +0530

    feat(js): solve largest of three numbers

---
+++ 03-javascript/01-operators-conditional-statements/05-largest-of-three.js
@@ -0,0 +1,13 @@
+// Qs5. Write a program to find the largest of 3 numbers.
+
+let a = 5;
+let b = 9;
+let c = 9;
+
+if (a >= b && a >= c) {
+  console.log(`${a} is largest.`);
+} else if (b >= c) {
+  console.log(`${b} is largest.`);
+} else {
+  console.log(`${c} is largest.`);
+}
