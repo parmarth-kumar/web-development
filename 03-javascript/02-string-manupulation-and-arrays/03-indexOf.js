827ae0a244217994ebbe7eb11b10a36d5a5c164f
Parmarth Kumar <parmarthk26@gmail.com>
2026-08-13 20:36:47 +0530

    feat(js-strings): add indexOf method for string searching

---
+++ 03-javascript/02-string-manupulation-and-arrays/03-indexOf.js
@@ -0,0 +1,6 @@
+let sentence =
+  "Artificial intelligence is the simulation of human intelligence processes by machines and computer systems.";
+
+console.log(sentence.indexOf("is")); // index when it first appeared
+console.log(sentence.indexOf("AI")); // -1
+console.log(sentence.indexOf("i")); // index when it first appeared
