184672b862b8cc77e4ad051f465715cc44885525
Parmarth Kumar <parmarthk26@gmail.com>
2026-08-18 17:39:33 +0530

    feat(javascript): add intro to array creation and basic properties

---
+++ 03-javascript/02-string-manupulation-and-arrays/07-creating-an-array.js
@@ -0,0 +1,13 @@
+let nums = [2, 4, 6, 8];
+console.log(nums);
+console.log(nums[0]);
+console.log(nums[1]);
+
+let arr = ["kunal", 9.6, 5, 99];
+console.log(arr[1]);
+console.log(typeof arr);
+console.log(arr.length);
+console.log(arr[0][0]);
+console.log(arr[0].length);
+
+let arr2 = []; //empty array
