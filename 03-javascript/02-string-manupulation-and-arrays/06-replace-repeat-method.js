b7e6a9aa7d1e95ad80b2a0ca7d93b76f0ffbaa4d
Parmarth Kumar <parmarthk26@gmail.com>
2026-08-18 17:36:31 +0530

    feat(javascript): add examples for string replace and repeat methods

---
+++ 03-javascript/02-string-manupulation-and-arrays/06-replace-repeat-method.js
@@ -0,0 +1,7 @@
+let msg = "IloveCoding";
+msg = msg.replace("love", "do");
+// msg = msg.replace("z", "do"); // no erroe if nothing to replace
+console.log(msg);
+
+let str = "Mango";
+console.log(str.repeat(3));
