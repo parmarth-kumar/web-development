0306c1399f80b396e510132d41f10c1891f084de
Parmarth Kumar <parmarthk26@gmail.com>
2026-08-19 22:50:44 +0530

    feat(javascript): demonstrate array search methods using indexOf and includes

---
+++ 03-javascript/02-string-manupulation-and-arrays/12-array-include-indexOf.js
@@ -0,0 +1,7 @@
+let primary = ["red", "yellow", "blue"];
+
+primary.indexOf("yellow"); //1 - returns index of item.
+primary.indexOf("Yellow"); //-1
+primary.indexOf("pink"); //-1
+
+primary.includes("red"); //true - checks if an item is present or not.
---
+++ 03-javascript/02-string-manupulation-and-arrays/13-array-includes-method.js
@@ -0,0 +1,4 @@
+let color = ["red", "green", "blue"];
+
+console.log(color.includes("blue")); // true if exist
+console.log(color.includes("black")); // false if not
