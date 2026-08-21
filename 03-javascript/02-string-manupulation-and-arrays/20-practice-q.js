a2438ff46c5b7cb27a620167b079cc278947e808
Parmarth Kumar <parmarthk26@gmail.com>
2026-08-21 20:44:29 +0530

    feat(javascript): add array reverse and indexOf practice problem solution

---
+++ 03-javascript/02-string-manupulation-and-arrays/20-practice-q.js
@@ -0,0 +1,10 @@
+// Qs. Return the index of the "javascript" from the given array, if it was reversed.
+
+// ['c', 'c++', 'html', 'javascript', 'python', 'java', 'c#', 'sql']
+
+let lang = ["c", "c++", "html", "javascript", "python", "java", "c#", "sql"];
+
+lang.reverse();
+console.log(lang);
+
+console.log(lang.indexOf("javascript"));
